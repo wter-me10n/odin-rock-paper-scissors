7e0b11f4113f5a97931adb29f6271ca2738472fb
Melon <148763683+wter-me10n@users.noreply.github.com>
2023-11-22 18:48:01 +0630

    Wrote function for playing a game round

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 function getComputerChoice(output) {
     let random = [
-        'Rock',
-        'Paper',
-        'Scissors'      
+        'rock',
+        'paper',
+        'scissors'      
     ]
 
     let choice = random[Math.floor(Math.random() * random.length)];
     output = choice;
     return output;
 };
+
+function playRound(playerSelection,computerSelection) {
+
+    if (playerSelection == 'rock' && computerSelection == 'rock') {
+        return 'Tie';
+        } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+            return 'Lose';
+        } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+            return 'Win';
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return 'Win';
+        } else if (playerSelection == 'paper' && computerSelection == 'paper') {
+            return 'Tie';
+        } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+            return 'Lose';
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return 'Lose';
+        } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+            return 'Win';
+        } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
+            return 'Tie';
+    }
+    
+}
+
+
+
